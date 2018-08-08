4a3568d685e39d468314ce564868357a3e054f2e
Ohad <ohadbaruch1@gmail.com>
2018-08-08 21:10:44 +0300

    Added weather API using Ajax call

---
+++ src/app/shopping-list/shopping-list-edit/weather.service.ts
@@ -0,0 +1,22 @@
+import { Subject } from 'rxjs/Subject';
+import { WeatherData } from '../../shared/weather-data.model';
+import { Http, Response } from '@angular/http';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class WeatherService{
+    weatherData : WeatherData;
+    weatherDataSubject = new Subject<WeatherData>();
+    constructor (private http: Http) {
+        this.weatherData = new WeatherData('');
+    };
+    
+    getWeather(){
+        var url = 'http://api.openweathermap.org/data/2.5/forecast?id=294640&APPID=6a0f2e776cccea51a5cec2d8264f5882&units=metric';
+        this.http.get(`${url}`).subscribe((response : Response) => {
+            var temp = response.json()['list'][0]['main']['temp'];
+            this.weatherData.temp = temp;
+            this.weatherDataSubject.next(this.weatherData);
+        });
+    }
+}
