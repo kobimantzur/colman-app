4c8c8dc8a8cf21fb437cdfacdfea2830fecff0cc
tomer13 <yomercohen13@gmail.com>
2018-08-08 21:00:08 +0300

    added the http requests to update and retrive data to and from the server on recipes

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -7,10 +7,10 @@ export class Recipe{
     public ingredients: Ingredient[];
 
 
-    constructor(name: string , desc: string, imagePath: string, ingredient: Ingredient[]){
+    constructor(name: string , desc: string, imagePath: string, ingredients: Ingredient[]){
         this.name = name;
         this.description = desc;
         this.imagePath = imagePath;
-        this.ingredients = ingredient; 
+        this.ingredients = ingredients; 
      }
 }
