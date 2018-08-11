c277b1527e6b0454cfef40845e67febdf9afc7d7
unknown <ohadbaruch1@gmail.com>
2018-08-11 15:44:54 +0300

    Fixed all bugs

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -34,7 +34,7 @@ export class RecipeListComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(){
-    this.subscription.unsubscribe();
+    // this.subscription.unsubscribe();
   }
 
   onItemClick(item) {
